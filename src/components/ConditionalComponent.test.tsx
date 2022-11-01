e77f4bf6eba76898908fb0cca0313dd85d87a450
davidalves1 <alves.david@outlook.com>
2022-11-01 23:38:38 +0000

    feat: add tests to ConditionalComponent

---
+++ src/components/ConditionalComponent.test.tsx
@@ -0,0 +1,27 @@
+import ConditionalComponent from './ConditionalComponent';
+import { render, screen } from '@testing-library/react';
+
+const mountComponent = (condition: boolean) =>
+  render(
+    <ConditionalComponent condition={condition} fallback={<span data-testid="fallback">Fallback</span>}>
+      <span data-testid="span">Test</span>
+    </ConditionalComponent>,
+  );
+
+describe('ConditionalComponent', () => {
+  it('should render the children if condition is true', () => {
+    mountComponent(true);
+
+    const span = screen.getByTestId('span');
+
+    expect(span).toBeInTheDocument();
+  });
+
+  it('should render the fallback when the condition is not satisfied and fallback is defined', () => {
+    mountComponent(false);
+
+    const fallback = screen.getByTestId('fallback');
+
+    expect(fallback).toBeInTheDocument();
+  });
+});
